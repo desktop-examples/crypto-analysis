195ab7f027182c71520fbaa2efd3c5c3ff415710
Mark McDowell <mark.mcdowell@reactivemarkets.com>
2020-04-29 20:47:18 +0100

    build(github): adding build for gh-pages
    Building static site.

--- src/ts/app/analysis.tsx
+++ src/ts/app/analysis.tsx
@@ -5,7 +5,6 @@ import { SideBar } from "./sideBar";
 import { VolumeAnalysis } from "./volumeAnalysis";
 
 export class Analysis extends React.PureComponent {
-
     public render() {
         return (
             <FlexView grow shrink basis="100%">
